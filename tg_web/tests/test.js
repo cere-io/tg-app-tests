f10d118f114f376f23049c1569b5632cc678fe63
VeronikaFil <veronika.filipenko@cere.io>
2025-02-24 18:25:34 +0300

    quests tab

--- tg_web/tests/test.js
+++ tg_web/tests/test.js
@@ -16,8 +16,24 @@ describe("Open active quests screen", async function () {
       "https://telegram-viewer-app.stage.cere.io/?campaignId=114"
     );
 
+    await driver.findElement(By.className("tgui-bca5056bf34297b0")).click();
+    await driver.findElement(By.className("welcom-cta-text")).click();
+
+    await driver.findElement(By.id("sign_in")).click();
     await driver.findElement(By.name("login")).sendKeys(userName);
-    await driver.findElement(By.name("password")).sendKeys(otp);
+    await driver.findElement(By.id(":r1:")).click();
+    await driver.findElement(By.id("otp")).sendKeys(otp);
+    await driver.findElement(By.id(":r3:")).click();
+
+    const questTitle = await driver
+      .findElement(By.className("t1uqjrzu"))
+      .getText();
+    expect(questTitle).to.equal("Complete Quests to Earn!");
+
+    const questTab = await driver
+      .findElement(By.className("tgui-e6658d0b8927f95e"))
+      .getText();
+    expect(questTab).to.equal("Active Quests");
   });
   after(async function () {
     await driver.quit();
